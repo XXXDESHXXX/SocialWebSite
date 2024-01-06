605e760ede47e529a28021603d3e1056a1baa931
XXXDESHXXX <kirya.shvarts@list.ru>
2024-01-06 18:32:17 +0300

    Create login.html, base.html templates; Create staticfile base.css; Crate urls.py

--- bookmarks/asgi.py
+++ bookmarks/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bookmarks project.
+ASGI config for bookmarks1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- bookmarks/wsgi.py
+++ bookmarks/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bookmarks project.
+WSGI config for bookmarks1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
